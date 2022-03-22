93134c8bf968f4da2feabe3b61ebf567787760a3
Lina <lnzitniak@gmail.com>
2022-03-23 00:44:16 +0200

    Assignment

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Cryptocurrency tax rate is 25% for short term capital gains in 2022
+
+        int cryptocurrency_value = 1234;
+        double tax_rate = cryptocurrency_value * 0.25;
+        System.out.println("cryptocurrency value * 0.25 = " + (tax_rate));
+
+    }
+}
